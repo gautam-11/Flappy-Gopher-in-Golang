2ebbd5a9704e0b87e5a0dbc3b548460ff5093454
Gautam Baruah <gautambaruah24@gmail.com>
2019-07-06 13:14:42 +0530

    Added speed to the bird and added bouncing feature

---
+++ bird.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	img "github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const gravity = 0.25
+
+type bird struct {
+	time     int
+	textures []*sdl.Texture
+	y, speed float64
+}
+
+func newBird(r *sdl.Renderer) (*bird, error) {
+
+	var textures []*sdl.Texture
+	for i := 1; i <= 4; i++ {
+		path := fmt.Sprintf("res/imgs/bird_frame_%d.png", i)
+		texture, err := img.LoadTexture(r, path)
+		if err != nil {
+			return nil, fmt.Errorf("could not load bird image: %v", err)
+		}
+		textures = append(textures, texture)
+	}
+	return &bird{textures: textures, y: 300}, nil
+}
+
+func (b *bird) paint(r *sdl.Renderer) error {
+
+	b.time++
+	b.y -= b.speed
+
+	if b.y < 0 {
+		b.y = 0
+		b.speed = -b.speed
+	}
+	b.speed += gravity
+
+	rect := &sdl.Rect{X: 10, Y: (600 - int32(b.y)) - 43/2, W: 50, H: 43}
+	i := b.time / 10 % len(b.textures)
+	if err := r.Copy(b.textures[i], nil, rect); err != nil {
+		return fmt.Errorf("could not copy background:%v", err)
+	}
+
+	return nil
+}
+
+func (b *bird) destroy() {
+	for _, t := range b.textures {
+		t.Destroy()
+	}
+}
--- scene.go
+++ scene.go
@@ -10,9 +10,8 @@ import (
 )
 
 type scene struct {
-	time  int
-	bg    *sdl.Texture
-	birds []*sdl.Texture
+	bird *bird
+	bg   *sdl.Texture
 }
 
 func newScene(r *sdl.Renderer) (*scene, error) {
@@ -21,16 +20,12 @@ func newScene(r *sdl.Renderer) (*scene, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not load background image: %v", err)
 	}
-	var birds []*sdl.Texture
-	for i := 1; i <= 4; i++ {
-		path := fmt.Sprintf("res/imgs/bird_frame_%d.png", i)
-		bird, err := img.LoadTexture(r, path)
-		if err != nil {
-			return nil, fmt.Errorf("could not load bird image: %v", err)
-		}
-		birds = append(birds, bird)
+
+	b, err := newBird(r)
+	if err != nil {
+		return nil, err
 	}
-	return &scene{bg: bg, birds: birds}, nil
+	return &scene{bg: bg, bird: b}, nil
 }
 
 func (s *scene) run(ctx context.Context, r *sdl.Renderer) <-chan error {
@@ -53,15 +48,12 @@ func (s *scene) run(ctx context.Context, r *sdl.Renderer) <-chan error {
 	return errc
 }
 func (s *scene) paint(r *sdl.Renderer) error {
-	s.time++
 	r.Clear()
 	if err := r.Copy(s.bg, nil, nil); err != nil {
 		return fmt.Errorf("could not copy background:%v", err)
 	}
-	rect := &sdl.Rect{X: 10, Y: 300 - 43/2, W: 50, H: 43}
-	i := s.time / 10 % len(s.birds)
-	if err := r.Copy(s.birds[i], nil, rect); err != nil {
-		return fmt.Errorf("could not copy background:%v", err)
+	if err := s.bird.paint(r); err != nil {
+		return err
 	}
 	r.Present()
 	return nil
@@ -69,4 +61,5 @@ func (s *scene) paint(r *sdl.Renderer) error {
 
 func (s *scene) destroy() {
 	s.bg.Destroy()
+	s.bird.destroy()
 }
